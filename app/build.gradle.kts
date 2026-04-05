73fc93856230f16741d98d53fed831f5e5c3e215
Kevin Burkart <kburkart@mugheadgolf.com>
2026-04-05 01:03:00 -0400

    set the correct api url

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         versionCode = 1
         versionName = "1.0"
 
-        buildConfigField("String", "API_BASE_URL", "\"http://10.0.2.2:8080/\"")
+        buildConfigField("String", "API_BASE_URL", "\"https://api.mugheadgolf.com/\"")
     }
 
     buildTypes {
